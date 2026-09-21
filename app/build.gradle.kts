2dfa3ea590ab2736e6486f8ada0215378738cfff
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2026-09-21 10:59:01 +0200

    Prepare Android 2026.9.21 build 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -282,7 +282,7 @@ android {
         applicationId = "dev.ipf.whitenoise.android"
         minSdk = 30
         targetSdk = 36
-        versionCode = 17
+        versionCode = 18
         versionName = "2026.9.21"
         manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher"
         manifestPlaceholders["appRoundIcon"] = "@mipmap/ic_launcher_round"
